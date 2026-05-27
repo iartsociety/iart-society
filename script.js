76b3dea10653dc70ee1619cb8a0397c6df698ac8
saraa-siva <studybutterfly100@gmail.com>
2026-05-27 13:54:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ import "./styles.css";
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
 }
+
+const addButton = document.getElementById("add-course-btn");
+
+addButton.addEventListener("click", () => {
+
+  const code = prompt("Enter Course Code:");
+
+  if (code === "DWHG26") {
+
+    let courses = JSON.parse(localStorage.getItem("myCourses")) || [];
+
+    if (!courses.includes("Diya Wall Hanging Guide")) {
+      courses.push("Diya Wall Hanging Guide");
+
+      localStorage.setItem("myCourses", JSON.stringify(courses));
+
+      alert("Course Added!");
+    } else {
+      alert("You already unlocked this course.");
+    }
+
+  } else {
+    alert("Invalid code.");
+  }
+
+});
